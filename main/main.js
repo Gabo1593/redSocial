6696f2d8d0dfd00b366b45aedf0fd0d61e6c007e
Gabo <gabrielfernandez873@gmail.com>
2023-06-22 22:07:18 -0500

    agregado fucnion de postear

--- main/main.js
+++ main/main.js
@@ -2,13 +2,22 @@ const X = (id) => document.querySelector(id);
 const botonPost = X(".botonPost");
 const text = X("#post");
 const divPost = X(".post");
+const sectionPost = X("#posted");
+const botonPosted = document.createElement("button");
+const newPost = document.createElement("h2");
+const textArea = document.createElement("textarea");
+
 
 botonPost.addEventListener("click", ()=>{console.log(text.value);
-const newPost = document.createElement("p");
+botonPosted.classList = "botonPosted"
+botonPosted.innerText = "comentar"
 const newPostValue = text.value;
-newPost.style.color = "red";
 newPost.classList = "newPostParrafo"
+sectionPost.classList = "posted";
 newPost.append(newPostValue);
-divPost.append(newPost);
+sectionPost.append(newPost, botonPosted);
 });
 
+// botonPosted.addEventListener("click", ()=>{console.log(text.value);
+//     textArea
+// });
