cd88fbe114c69f51bee018c96347cb0b029fad90
Gabo <gabrielfernandez873@gmail.com>
2023-06-24 13:17:14 -0500

    agregados imagenes de perfil en el post y header

--- main/main.js
+++ main/main.js
@@ -3,13 +3,16 @@ const botonPost = X(".botonPost");
 const text = X("#post");
 const divPost = X(".post");
 const sectionPost = X("#posted");
+let imagePreview = X("#imagePreview");
 
-
-botonPost.addEventListener("click", ()=>{console.log(text.value);
+botonPost.addEventListener("click", ()=>{
 const botonPosted = document.createElement("button");
 const newPost = document.createElement("h2");
 const textArea = document.createElement("input");
 const postedDiv = document.createElement("div");
+let imgPerfil = document.createElement("img");
+imgPerfil.classList = "imgPerfil";
+imgPerfil.src = imagePreview.src;
 
 postedDiv.classList = "postedDiv";    
 botonPosted.classList = "botonPosted";
@@ -19,7 +22,7 @@ newPost.classList = "newPostParrafo";
 sectionPost.classList = "posted";
 newPost.append(newPostValue);
 text.value = "";
-postedDiv.append(newPost, textArea, botonPosted);
+postedDiv.append(imgPerfil, newPost, textArea, botonPosted);
 sectionPost.append(postedDiv);
 //funtion botonComentario
 botonPosted.addEventListener("click",crearComentario );
@@ -54,3 +57,22 @@ function crearComentario(){
 
 
 
+
+//miniatura al caragar la imagen
+function previewImage () {
+    
+    const form = document.getElementById("uploadForm");
+    const reader = new FileReader();
+    const filePreview = document.querySelector('#file').files[0];
+
+    reader.addEventListener('load', () => {
+  
+        imagePreview.src = reader.result; 
+    }, false);
+  
+    if(filePreview) {
+        reader.readAsDataURL(filePreview)
+    }
+        form.style.display = "none";
+  }
+  
