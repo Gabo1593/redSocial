9a3145227fd5638a5efda8794632e0dfc4662119
Gabo <gabrielfernandez873@gmail.com>
2023-06-28 11:18:54 -0500

    agregada las funciones de subir imagenes en los post y las miniaturas

--- main/main.js
+++ main/main.js
@@ -6,56 +6,70 @@ const sectionPost = X("#posted");
 let imagePreview = X("#imagePreview");
 const galeria = X(".galeria");
 const labelGaleria = X(".label--galeria");
+let imagenPreviaPost = X(".imagenPrevia");
+
+
+
 
 botonPost.addEventListener("click", ()=>{
-const botonPosted = document.createElement("button");
-const newPost = document.createElement("h2");
-const textArea = document.createElement("input");
-const postedDiv = document.createElement("div");
-let imgPerfil = document.createElement("img");
-imgPerfil.classList = "imgPerfil";
-imgPerfil.src = imagePreview.src;
-
-postedDiv.classList = "postedDiv";    
-botonPosted.classList = "botonPosted";
-botonPosted.innerText = "comentar";
-const newPostValue = text.value;
-newPost.classList = "newPostParrafo";
-sectionPost.classList = "posted";
-newPost.append(newPostValue);
-text.value = "";
-postedDiv.append(imgPerfil, newPost, textArea, botonPosted);
-sectionPost.append(postedDiv);
-//funtion botonComentario
-botonPosted.addEventListener("click",crearComentario );
-
-function crearComentario(){
-    const botonComentar = document.createElement("button");
-    botonComentar.classList = "botonComentar";
-    botonComentar.innerText = "Responder"
-    const newComentario = document.createElement("p");
-    const newComentarioValue = textArea.value;
-    newComentario.append(newComentarioValue);
-    textArea.value = "";
-    const inputComentario = document.createElement("input");
-    const divComentario = document.createElement("div");
-    divComentario.classList = "divComentario";
-    divComentario.append(newComentario, inputComentario, botonComentar);
-    postedDiv.append(divComentario);
-    botonComentar.addEventListener("click", respuestaComentario);
-    
-    function respuestaComentario(){
-        const newRespuesta = document.createElement("p");
-        const newRespuestaValue = inputComentario.value;
-        newRespuesta.append(newRespuestaValue);
-        inputComentario.value = "";
-        const divRespuesta = document.createElement("div");
-        divRespuesta.classList = "divRespuesta";
-        divRespuesta.append(newRespuesta);
-        inputComentario.insertAdjacentElement("beforeBegin", divRespuesta)
+    const botonPosted = document.createElement("button");
+    const newPost = document.createElement("h2");
+    const textArea = document.createElement("input");
+    const postedDiv = document.createElement("div");
+    let imgPerfil = document.createElement("img");
+    imgPerfil.classList = "imgPerfil";
+    imgPerfil.src = imagePreview.src;
+   
+    let imgPost = document.createElement("img");
+    imgPost.classList = "imgPost";
+    imgPost.src =  imagenPreviaPost.src;
+   
+    postedDiv.classList = "postedDiv";    
+    botonPosted.classList = "botonPosted";
+    botonPosted.innerText = "comentar";
+    const newPostValue = text.value;
+    newPost.classList = "newPostParrafo";
+    sectionPost.classList = "posted";
+    newPost.append(newPostValue);
+    text.value = "";
+    if(imgPost.src === "http://127.0.0.1:5500/icons/icons-image-negro-64.png"){
+        postedDiv.append(imgPerfil, newPost, textArea, botonPosted);
+    }else{
+        postedDiv.append(imgPerfil, newPost, imgPost, textArea, botonPosted);
     }
-}
-});
+    imagenPreviaPost.src = "../icons/icons-image-negro-64.png";
+    sectionPost.append(postedDiv)
+    
+    //funtion botonComentario
+    botonPosted.addEventListener("click",crearComentario );
+
+    function crearComentario(){
+        const botonComentar = document.createElement("button");
+        botonComentar.classList = "botonComentar";
+        botonComentar.innerText = "Responder"
+        const newComentario = document.createElement("p");
+        const newComentarioValue = textArea.value;
+        newComentario.append(newComentarioValue);
+        textArea.value = "";
+        const inputComentario = document.createElement("input");
+        const divComentario = document.createElement("div");
+        divComentario.classList = "divComentario";
+        divComentario.append(newComentario, inputComentario, botonComentar);
+        postedDiv.append(divComentario);
+        botonComentar.addEventListener("click", respuestaComentario);
+    
+        function respuestaComentario(){
+            const newRespuesta = document.createElement("p");
+            const newRespuestaValue = inputComentario.value;
+            newRespuesta.append(newRespuestaValue);
+            inputComentario.value = "";
+            const divRespuesta = document.createElement("div");
+            divRespuesta.classList = "divRespuesta";
+            divRespuesta.append(newRespuesta);
+            inputComentario.insertAdjacentElement("beforeBegin", divRespuesta)
+        }
+    }
+}); 
 
 
 
@@ -63,7 +77,6 @@ function crearComentario(){
 //miniatura al caragar la imagen
 function previewImage () {
     
-    const form = document.getElementById("uploadForm");
     const reader = new FileReader();
     const filePreview = document.querySelector('#file').files[0];
 
@@ -75,19 +88,39 @@ function previewImage () {
     if(filePreview) {
         reader.readAsDataURL(filePreview)
     }
-    form.style.display = "none";
-    imagePreview.addEventListener("click", ()=>{
-    form.style.display = "flex";
-    })
 
 }
   
-labelGaleria.addEventListener("click", crearGaleria);
 
 function crearGaleria(){
     const imagenGaleria = document.createElement("img");
     imagenGaleria.classList = "imagenGaleria";
-    imagenGaleria.src = "https://img3.wallspic.com/previews/5/1/1/3/7/173115/173115-anime-linda_chica_anime-chica_gamer-chica_chica-kawaii-500x.jpg";
+    const reader = new FileReader();
+    const filePreview = document.querySelector("#for--galeria").files[0];
+    reader.addEventListener('load', () => {
+  
+        imagenGaleria.src = reader.result; 
+    }, false);
+    if(filePreview) {
+        reader.readAsDataURL(filePreview)
+    }
     galeria.append(imagenGaleria);
+}
 
-  }
+function imagenPost(){
+    
+
+    const reader = new FileReader();
+    const filePreview = document.querySelector("#imagePost").files[0];
+    reader.addEventListener('load', () => {
+  
+        imagenPreviaPost.src = reader.result; 
+        // imgPost.src  = reader.result; 
+    }, false);
+    if(filePreview) {
+        reader.readAsDataURL(filePreview)
+    }
+
+   
+
+}
